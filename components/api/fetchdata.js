950215c43cf2456376e2a674fd06837d1a6d7304
David <muriithid05@gmail.com>
2023-04-15 12:48:55 +0000

    modify .gitignore

--- components/api/fetchdata.js
+++ components/api/fetchdata.js
@@ -1,21 +1,23 @@
 import axios from 'axios'
 
+export const papikey = '24d76c42-cfa8 - 431f - bda7 - 6a8773e30880'
+export const gqlapi = 'https://justinedev.verixr.com/api/graphql'
 export const fetcher = (path) => axios.get(path, {
     headers: {
-        "Authorization": "account API-Key 24d76c42-cfa8-431f-bda7-6a8773e30880"
+        "Authorization": `account API-Key ${papikey}`
     }
 }).then(res => res)
 
 
 export const sender = (path) => axios.post(path, {
     headers: {
-        "Authorization": "account API-Key 24d76c42-cfa8-431f-bda7-6a8773e30880"
+        "Authorization": `account API-Key ${papikey}`
     }
 })
 
 export const adminfetcher = (path) => axios.get(path, {
     headers: {
-        "Authorization": "account API-Key 24d76c42-cfa8-431f-bda7-6a8773e30880"
+        "Authorization": `account API-Key ${papikey}`
     }
 }).then(res => res)
 
