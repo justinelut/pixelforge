292d79c9a52412e83a816b1c94020867ee838494
David <muriithid05@gmail.com>
2023-04-15 12:52:21 +0000

    Editing the fetchdata function

--- components/api/fetchdata.js
+++ components/api/fetchdata.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const papikey = '24d76c42-cfa8 - 431f - bda7 - 6a8773e30880'
+export const papikey = '24d76c42-cfa8-431f-bda7-6a8773e30880'
 export const gqlapi = 'https://justinedev.verixr.com/api/graphql'
 export const fetcher = (path) => axios.get(path, {
     headers: {
